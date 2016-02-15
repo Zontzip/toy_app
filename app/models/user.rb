fd1102321ee65039ea7d342a79d17b7329127b6d
Alex <zontzor@gmail.com>
2016-02-15 13:21:38 +0000

    Add user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 # Users inherits from ActiveRecord and uses its .All method to fetch from db
 class User < ActiveRecord::Base
+  # Matches users to microposts in database
   has_many :microposts
+  # Fields cannot be blank 
+  validates FILL_IN, presence: true
+  validates FILL_IN, presence: true
 end
